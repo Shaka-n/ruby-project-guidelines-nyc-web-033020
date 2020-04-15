81f6d2f2c804ba004a63d25a14a3fb79034360c8
Garen Sahagian <g.s.sahagian@gmail.com>
2020-04-15 10:37:00 -0400

    adding more game file

--- bin/game.rb
+++ bin/game.rb
@@ -1,1 +1,12 @@
 require_relative '../config/environment'
+
+puts "Hello, welcome to our game. You've been inside for a while now.
+    Maybe it's time for a walk. You'll need your gloves and facemask 
+    before you can go outside. Before we start, what's your name?"
+player_name = gets.chomp
+
+#create new player instance here using player_name
+
+puts "Thank you. Welcome to the game #{player_name}."
+
+puts "You're sitting at your desk."
--- lib/room.rb
+++ lib/room.rb
@@ -1,4 +1,5 @@
 class Room < ActiveRecord::Base
     has_many :adjacents, class_name: "Room", foreign_key: ":proximal_room_id"
     belongs_to :proximal_room, class_name: "Room", optional:true
+    has_many :items, as: :grabbable
 end
