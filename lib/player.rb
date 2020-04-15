52295f4c189c0e0e05f9aee4da2ceddd6f59c3ad
Garen Sahagian <g.s.sahagian@gmail.com>
2020-04-15 15:25:07 -0400

    Attempting to remove conflicts.

--- lib/player.rb
+++ lib/player.rb
@@ -1,23 +1,6 @@
 class Player < ActiveRecord::Base
     has_many :items, as: :grabbable
 
-    def decision_loop
-        puts "What would you like to do?"
-        puts"1. Move
-        2. Search/Interact
-        3. Check Inventory"
-        input = gets.chomp
-        if input = 1 
-            # Display Available Rooms
-        elsif input = 2
-            # Display Available Interactions
-        elsif input = 3
-            # Display Items That Belong to Player
-        else
-            puts "Please use the number keys to indicate your choice."
-        end
-    end
-
     def available_rooms
         current_room = Room.all.find(id == self.room_id)
         options = Room.all.select do |r|
